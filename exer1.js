bdda549491f23f072a45f5ebf5275b8062136adb
Matheus Soares <106754355+theussoares@users.noreply.github.com>
2024-11-09 23:00:39 -0400

    Create exer1.js

---
+++ exer1.js
@@ -0,0 +1,23 @@
+//oneThroughTwenty - Retornar os números de 1 a 20. (1, 2, 3,..., 19, 20)
+//evensToTwenty - Retornar os números pares de 1 a 20. (2, 4, 6,..., 18, 20)
+//oddsToTwenty - Retornar os números ímpares de 1 a 20. (1, 3, 5,..., 17, 19)
+//multiplesOfFive - Retornar os múltiplos de 5 até 100. (5, 10, 15,..., 95, 100)
+//squareNumbers - Retornar todos os números até 100 que forem quadrados perfeitos. (1, 4, 9, ..., 81, 100)
+//countingBackwards - Retornar os números contando de trás para frente de 20 até 1. (20, 19, 18, ..., 2, 1)
+//evenNumbersBackwards - Retornar os números pares de 20 até 1. (20, 18, 16, ..., 4, 2)
+//oddNumbersBackwards - Retornar os números ímpares de 20 até 1. (19, 17, 15, ..., 3, 1)
+//multiplesOfFiveBackwards - Retornar os múltiplos de 5 contando de trás para frente a partir de 100. (100, 95, 90, ..., 10, 5)
+//squareNumbersBackwards - Retornar os quadrados perfeitos contando de trás para frente a partir de 100. (100, 81, 64, ..., 4, 1)
+
+function oneThroughFive() {
+
+    let meuRetorno = [];
+
+    for (let counter = 1; counter <= 5; counter++) {
+        meurRetorno.push(counter)
+    }
+
+    return meuRetorno;
+}
+
+console.log(oneThroughFive());
